f16099474aae8ca3d2b299126821347eb0eb7047
anti256 <anti256@mail.ru>
2020-11-03 00:00:07 +0300

    Homework 4.2_1

---
+++ 04_NumbersStringsAndDates/homework_4.2/src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+
+    public static void main(String[] args) {
+	  //byte
+    System.out.println("Максимальное отрицательное byte: " + Byte.MIN_VALUE);
+    System.out.println("Максимальное положительное byte: " + Byte.MAX_VALUE);
+
+    //int
+    System.out.println("\nМаксимальное отрицательное int: " + Integer.MIN_VALUE);
+    System.out.println("Максимальное положительное int: " + Integer.MAX_VALUE);
+
+    //short
+    System.out.println("\nМаксимальное отрицательное short: " + Short.MIN_VALUE);
+    System.out.println("Максимальное положительное short: " + Short.MAX_VALUE);
+
+    //long
+    System.out.println("\nМаксимальное отрицательное long: " + Long.MIN_VALUE);
+    System.out.println("Максимальное положительное long: " + Long.MAX_VALUE);
+
+    //float
+    System.out.println("\nМаксимальное отрицательное float: " + -Float.MAX_VALUE);
+    System.out.println("Минимальное отрицательное float: " + -Float.MIN_VALUE);
+    System.out.println("Минимальное положительное float: " + Float.MIN_VALUE);
+    System.out.println("Максимальное положительное float: " + Float.MAX_VALUE);
+
+    //double
+    System.out.println("\nМаксимальное отрицательное double: " + -Double.MAX_VALUE);
+    System.out.println("Минимальное отрицательное double: " + -Double.MIN_VALUE);
+    System.out.println("Минимальное положительное double: " + Double.MIN_VALUE);
+    System.out.println("Максимальное положительное double: " + Double.MAX_VALUE);
+    }
+}
