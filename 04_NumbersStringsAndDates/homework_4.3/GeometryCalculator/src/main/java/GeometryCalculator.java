e02c5c9b4087044aea6de2755146ca93ac80105d
anti256 <anti256@mail.ru>
2020-11-05 22:40:21 +0300

    Homework 4.3

--- 04_NumbersStringsAndDates/homework_4.3/GeometryCalculator/src/main/java/GeometryCalculator.java
+++ 04_NumbersStringsAndDates/homework_4.3/GeometryCalculator/src/main/java/GeometryCalculator.java
@@ -1,21 +1,28 @@
 public class GeometryCalculator {
-    // метод должен использовать абсолютное значение radius
-    public static double getCircleSquare(double radius) {
-        return 0;
-    }
 
-    // метод должен использовать абсолютное значение radius
-    public static double getSphereVolume(double radius) {
-        return 0;
-    }
+  // метод должен использовать абсолютное значение radius
+  public static double getCircleSquare(double radius) {
+    return Math.PI * Math.pow(Math.abs(radius), 2);
+  }
 
-    public static boolean isTrianglePossible(double a, double b, double c) {
-        return false;
-    }
+  // метод должен использовать абсолютное значение radius
+  public static double getSphereVolume(double radius) {
+    return 4 * Math.PI * Math.pow(Math.abs(radius), 3) / 3;
+  }
 
-    // перед расчетом площади рекомендуется проверить возможен ли такой треугольник
-    // методом isTrianglePossible, если невозможен вернуть -1.0
-    public static double getTriangleSquare(double a, double b, double c) {
-        return 0;
-    }
+  public static boolean isTrianglePossible(double a, double b, double c) {
+
+    return (((a + b - c) > 0) & ((c + b - a) > 0) & ((a + c - b) > 0));
+  }
+
+  // перед расчетом площади рекомендуется проверить возможен ли такой треугольник
+  // методом isTrianglePossible, если невозможен вернуть -1.0
+  public static double getTriangleSquare(double a, double b, double c) {
+      if (isTrianglePossible(a, b, c)) {
+          double p = (a + b + c) / 2;
+          return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+      } else {
+          return -1.0D;
+      }
+  }
 }
