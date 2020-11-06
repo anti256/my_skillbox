884aab5038aced14341e1807889dcb8bdbd2e0df
anti256 <anti256@mail.ru>
2020-11-06 23:04:49 +0300

    Homework 4.3 add1

--- 04_NumbersStringsAndDates/homework_4.3/TrucksContainersAndBoxes/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.3/TrucksContainersAndBoxes/src/main/java/Main.java
@@ -3,10 +3,43 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        //System.out.println("Введите количество ящиков");
         Scanner scanner = new Scanner(System.in);
         String boxes = scanner.nextLine();
+        int cases = Integer.parseInt(boxes);
+        int truck = 0, container = 0, currentCase = 1;
 
-        // TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
+        if (Math.abs(cases) == cases){// проверка корректности введенного числа ящиков
+          if (cases > 0) {//если ящиков больше нуля
+            truck = 1;    //начальное количество грузовиков и контейнеров равно единице
+            container = 1;
+            System.out.println("Грузовик: " + truck);
+            System.out.println("\tКонтейнер: " + container);
+            while (currentCase < (cases + 1)) {//начало цикла по ящикам
+
+              if ((currentCase > 1) & ((currentCase - 1) % 27
+                  == 0)) {//если количество ящиков больше 27
+                container++;                                         //берем следующий контейнер
+                if ((container > 1) & ((container - 1) % 12
+                    == 0)) {    //если количество контейнеров больше 12
+                  truck++;                                           //берем следующий грузовик
+                  System.out.println("Грузовик: " + truck);
+                }
+                System.out.println("\tКонтейнер: " + container);
+              }
+
+              System.out.println("\t\tЯщик: " + currentCase);
+              currentCase++;
+            }
+          }
+
+            System.out.println("Необходимо:");
+            System.out.println("грузовиков - " + truck + " шт.");
+            System.out.println("контейнеров - " + container + " шт.");
+        }
+        else {System.out.println("Количество ящиков должно быть не отрицательное.");}
+
+
         // пример вывода при вводе 2
         // для отступа используйте табуляцию - \t
 
