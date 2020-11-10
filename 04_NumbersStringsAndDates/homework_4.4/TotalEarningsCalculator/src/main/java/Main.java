b28d4e7603d849f106975bca5416a2e621a3ae42
anti256 <anti256@mail.ru>
2020-11-10 22:43:57 +0300

    Homework 4.4 TotalEarningsCalculator

--- 04_NumbersStringsAndDates/homework_4.4/TotalEarningsCalculator/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/TotalEarningsCalculator/src/main/java/Main.java
@@ -3,7 +3,19 @@ public class Main {
   public static void main(String[] args) {
 
     String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-    //TODO: напишите ваш код, результат вывести в консоль
+    int spaceIndex, totalMoney = 0;
+    String textCut;
+    char firstChar;
+
+    while (text.indexOf(" ") > -1) {
+      spaceIndex = text.indexOf(" ");
+      textCut = text.substring(0, spaceIndex);
+      text = text.substring(spaceIndex + 1);
+      firstChar = textCut.charAt(0);
+      if (Character.isDigit(firstChar)) {totalMoney = totalMoney + Integer.parseInt(textCut);}
+    }
+
+    System.out.println(totalMoney);
   }
 
 }
