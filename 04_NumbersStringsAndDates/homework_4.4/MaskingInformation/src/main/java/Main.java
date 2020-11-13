cf4d7045165a18895f80e3b35f4017e15e43ee46
anti256 <anti256@mail.ru>
2020-11-13 22:31:36 +0300

    Homework 4.4 MaskingInformation

--- 04_NumbersStringsAndDates/homework_4.4/MaskingInformation/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/MaskingInformation/src/main/java/Main.java
@@ -5,7 +5,17 @@ public class Main {
     }
 
     public static String searchAndReplaceDiamonds(String text, String placeholder) {
-        // TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
+        int secondDiamondsIndex = 1;
+        while (secondDiamondsIndex > 0) {
+            int firstDiamondsIndex = text.indexOf("<");
+            secondDiamondsIndex = text.indexOf(">");
+            String firstString, secondString;
+            if ((firstDiamondsIndex > 0) & (secondDiamondsIndex > 0)) {
+                firstString = text.substring(0, firstDiamondsIndex);
+                secondString = text.substring(secondDiamondsIndex + 1);
+                text = firstString + placeholder + secondString;
+            }
+        }
         return text;
     }
 
