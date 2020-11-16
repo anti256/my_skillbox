856d33dc2e48175d8481ef1776665b1637d1868c
anti256 <anti256@mail.ru>
2020-11-16 23:29:13 +0300

    Homework 4.4 TotalEarningsCalculatorReqExp

--- 04_NumbersStringsAndDates/homework_4.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
@@ -4,9 +4,16 @@ public class Main {
 
   }
 
-  public static int calculateSalarySum(String text){
-    //TODO: реализуйте метод
-    return -1;
+  public static int calculateSalarySum(String text) {
+    int summ = 0;//начальная сумма
+    text = text.replaceAll("[^0-9]", " ");//заменяем все кроме чисел пробелами
+    text = text.trim();//удаляем пробелы по бокам строки
+    String[] strArray = text.split("\\s+");//делим строку пробелами
+    if (!strArray[0].equals("")) {//если в строке не было чисел, первый элемент будет пустой строкой
+      //складываем "числа" в массиве
+      for (int i = 0; i < strArray.length;){summ += Integer.parseInt(strArray[i++]);}
+    }
+    return summ;
   }
 
 }
