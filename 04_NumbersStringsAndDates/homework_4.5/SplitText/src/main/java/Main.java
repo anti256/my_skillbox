923ce0b3353acddede60f3b6356e2cace9d6cc0c
anti256 <anti256@mail.ru>
2020-11-18 22:02:50 +0300

    Homework 4.5 SplitText

--- 04_NumbersStringsAndDates/homework_4.5/SplitText/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/SplitText/src/main/java/Main.java
@@ -5,8 +5,11 @@ public class Main {
   }
 
   public static String splitTextInToWords(String text) {
-    //TODO реализуйте метод
-    return "";
+    text = text.replaceAll("[^’'a-zA-Z]", " ");//заменяем все "небуквы" и "неапострофы" на пробелы
+    text = text.replaceAll("\\s+", " ");//заменяем последовательности пробелов одним пробелом
+    text = text.trim();//убираем лишние пробелы по краям строки
+    text = text.replaceAll("\\s", "\r\n");//заменяем пробелы переводом на следующую строку
+    return text;
   }
 
 }
