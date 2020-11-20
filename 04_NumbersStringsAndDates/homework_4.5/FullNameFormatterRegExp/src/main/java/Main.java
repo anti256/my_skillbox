8121d1df17b3052813f77dbc7e59cc348bcbf705
anti256 <anti256@mail.ru>
2020-11-21 00:17:05 +0300

    Homework 4.5 FullNameFormatterRegExp

--- 04_NumbersStringsAndDates/homework_4.5/FullNameFormatterRegExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/FullNameFormatterRegExp/src/main/java/Main.java
@@ -2,6 +2,14 @@ import java.util.Scanner;
 
 public class Main {
 
+  private static Boolean isValidText(String stroka) {//метод проверки валидности
+    if (stroka.equals("")) {return false;}//если строка пустая сразу возвращает false
+    //вычитаем из строки валидное ФИО
+    stroka = stroka.replaceAll("([А-ЩЭЮЯ][а-я]*(-[А-ЩЭЮЯ])?[а-я]*\\s){2}[А-ЩЭЮЯ][а-я]*-?[а-я]*", "");
+    if (stroka.equals("")) {return true;}//после вычитания строка должна стать пустой, тогда метод возвращает true
+    return false;//значение по умолчанию
+  }
+
   public static void main(String[] args) {
 
     Scanner scanner = new Scanner(System.in);
@@ -10,8 +18,17 @@ public class Main {
       if (input.equals("0")) {
         break;
       }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+      input = input.trim();//убираем пробелы по краям строки
+
+      if (isValidText(input)) {//проверяем валидность ФИО
+        //если ФИО валидное
+        String[] strArray = input.split(" ");//режем строку по пробелам
+        //выводим строками ФИО
+        System.out.println("Фамилия: " + strArray[0]);
+        System.out.println("Имя: " + strArray[1]);
+        System.out.println("Отчество: " + strArray[2]);
+        //если ФИО не валидное, выводим сообщение об этом
+      } else {System.out.println("Введенная строка не является ФИО");}
     }
   }
 
