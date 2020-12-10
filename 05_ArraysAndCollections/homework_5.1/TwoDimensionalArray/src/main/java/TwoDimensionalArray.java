98a01202983c5a7f7215933d32d3efe185fd117b
anti256 <anti256@mail.ru>
2020-12-10 23:03:15 +0300

    Homework 5.1 TwoDimensionalArray

--- 05_ArraysAndCollections/homework_5.1/TwoDimensionalArray/src/main/java/TwoDimensionalArray.java
+++ 05_ArraysAndCollections/homework_5.1/TwoDimensionalArray/src/main/java/TwoDimensionalArray.java
@@ -2,13 +2,20 @@ public class TwoDimensionalArray {
     public static char symbol = 'X';
 
     public static char[][] getTwoDimensionalArray(int size) {
-
-        //TODO: Написать метод, который создаст двумерный массив char заданного размера.
+        char[][] charMassiv = new char[size][size];//объявляем массив
+        for (int i = 0; i < size; i++){//перебор столбцов
+            for (int j = 0 ; j <size; j++){//перебор строк в столбце
+            charMassiv[i][j] = ' ';//заполняем всю строку пробелами
+        }
+            charMassiv[i][i] = symbol;//диагональ слева-направо
+            charMassiv[i][size - 1 - i] = symbol;//диагональ справа-налево
+        }
+        //Написать метод, который создаст двумерный массив char заданного размера.
         // массив должен содержать символ symbol по диагоналям, пример для size = 3
         // [X,  , X]
         // [ , X,  ]
         // [X,  , X]
 
-        return new char[0][0];
+        return charMassiv;
     }
 }
