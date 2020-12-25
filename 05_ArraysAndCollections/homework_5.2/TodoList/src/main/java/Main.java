4d7c621967e71dea0f77d9b9b87d8ca5fd3b05f7
anti256 <anti256@mail.ru>
2020-12-25 22:49:18 +0300

    HomeWork 4.6 ArrayList

--- 05_ArraysAndCollections/homework_5.2/TodoList/src/main/java/Main.java
+++ 05_ArraysAndCollections/homework_5.2/TodoList/src/main/java/Main.java
@@ -1,7 +1,93 @@
+import java.util.Scanner;
+
 public class Main {
     private static TodoList todoList = new TodoList();
 
+   //обработка по слову ADD
+    private static void addToArrayList (String[] strArray){
+        //если введеных слов минимум два и второе это число и это число положительное
+        if ((strArray.length > 1) && (strArray[1].matches("\\d+"))
+            && (Integer.parseInt(strArray[1]) >= 0) ) {
+          int pos = Integer.parseInt(strArray[1]); //сохраняем позицию
+          //"уничтожаем" служебные слова
+          strArray[0] = "";
+          strArray[1] = "";
+          //пересобираем строку из массива - остается только строка, которую нужно добавить в список
+          String stroka = String.join(" ", strArray);
+          stroka = stroka.trim();//первые два пустые элементы добавляют лишние пробелы, убираем их
+          //если позиции не существует, добавляем в конец списка
+          if (pos >= todoList.getTodos().size()){todoList.add(stroka);}
+          //иначе добавляем в указанную позицию
+          else {todoList.add(pos, stroka);}
+         }
+        //если не выполняется хотя бы одно из трех условий
+        else {
+        strArray[0] = "";//убираем слово ADD
+        String stroka = String.join(" ", strArray);//пересобираем строку
+        stroka = stroka.trim();//удаляем лишние пробелы
+        todoList.add(stroka);//долбавляем строку в конец списка
+      }
+    }
+
+    //обработка по слову EDIT
+    private static void editPosArrayList(String[] strArray){
+      //если второе слово - число и оно положительное
+      if (strArray[1].matches("\\d+") && (Integer.parseInt(strArray[1]) >= 0)){
+      int pos = Integer.parseInt(strArray[1]);//сохраняем позицию
+      //"уничтожаем" служебные слова
+      strArray[0] = "";
+      strArray[1] = "";
+      //пересобираем строку из массива - остается только нстрока под замену в списке
+      String stroka = String.join(" ", strArray);
+      stroka = stroka.trim();//первые два пустые элементы добавляют лишние пробелы, убираем их
+      todoList.edit(stroka, pos);}}//заменяем строку по позиции
+
     public static void main(String[] args) {
-        // TODO: написать консольное приложение для работы со списком дел todoList
+
+      //информация
+      System.out.println("Введите команду в одном из форматов:" + System.lineSeparator() +
+          "ADD строка" + System.lineSeparator() + "ADD номер_позиции строка" +
+          System.lineSeparator() + "EDIT номер_позиции строка" + System.lineSeparator()
+          + "LIST" + System.lineSeparator() + "DELETE номер_позиции" + System.lineSeparator()
+          + "нумерация позиций начинается с нуля" + System.lineSeparator() + "0 - выход");
+      Scanner scanner = new Scanner(System.in);
+        while (true) {//бесконечный цикл
+          String input = scanner.nextLine();//сканер по строкам
+          if (input.equals("0")) {//выход из цикла/программы по введению нуля
+            break;
+          }
+
+          //преобразуем введенное в массив строк по разделителю пробелу
+          String[] strokaArray = input.split(" ");
+
+          //если первое введеное слово ADD
+          if (strokaArray[0].equals("ADD")) {
+            //передаем в обработку массив введеных слов
+            addToArrayList(strokaArray);
+          }
+
+          //если первое введеное слово EDIT
+          if (strokaArray[0].equals("EDIT")) {
+            //передаем в обработку массив введеных слов
+            editPosArrayList(strokaArray);
+          }
+
+          //если первое введеное слово LIST
+          if (strokaArray[0].equals("LIST")) {
+            //выводим список циклом for each
+            for (String s : todoList.getTodos()) {
+              System.out.println(s);
+            }
+          }
+
+          //если первое введеное слово DELETE
+          if (strokaArray[0].equals("DELETE")) {
+            //проверка, что слов больше одного и второе "слово" - число
+            if ((strokaArray.length > 1) && (strokaArray[1].matches("\\d+"))) {
+              //передаем в экземпляр класса второе слово-число, как аргумент
+              todoList.delete(Integer.parseInt(strokaArray[1]));
+            }
+          }
+        }
     }
 }
--- 05_ArraysAndCollections/homework_5.2/TodoList/src/main/java/TodoList.java
+++ 05_ArraysAndCollections/homework_5.2/TodoList/src/main/java/TodoList.java
@@ -1,29 +1,28 @@
 import java.util.ArrayList;
 
-public class TodoList {
+public class TodoList{
+
+   ArrayList<String> list = new ArrayList<>();
 
     public void add(String todo) {
-        // TODO: добавьте переданное дело в конец списка
+        list.add(todo);
     }
 
     public void add(int index, String todo) {
-        // TODO: добавьте дело на указаный индекс,
-        //  проверьте возможность добавления
+        if (index < list.size()) {list.add(index, todo);}
+        else {list.add(todo);}
     }
 
     public void edit(String todo, int index) {
-        // TODO: заменить дело на index переданным todo индекс,
-        //  проверьте возможность изменения
+        if (index < list.size()) {list.set(index, todo);}
     }
 
     public void delete(int index) {
-        // TODO: удалить дело находящееся по переданному индексу,
-        //  проверьте возможность удаления дела
+        if (index < list.size()){list.remove(index);}
     }
 
     public ArrayList<String> getTodos() {
-        // TODO: вернуть список дел
-        return new ArrayList<>();
+        return list;
     }
 
 }
