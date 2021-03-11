e327683cd15a4c0f21383ab1946a004a6211a0bd
anti256 <anti256@mail.ru>
2021-03-11 22:19:39 +0300

    HomeWork 7.1 LambdaExpressions

--- 07_AdvancedOOPFeatures/homework_7.1/src/main/java/Main.java
+++ 07_AdvancedOOPFeatures/homework_7.1/src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
@@ -10,6 +12,10 @@ public class Main {
     }
 
     public static void sortBySalaryAndAlphabet(List<Employee> staff) {
-        //TODO Метод должен отсортировать сотрудников по заработной плате и алфавиту.
+        Collections.sort(staff, (o1, o2) -> {
+            int compareResult = o1.getSalary().compareTo(o2.getSalary());
+            if (compareResult == 0) {return o1.getName().compareTo(o2.getName());}
+            return compareResult;
+        });
     }
 }
